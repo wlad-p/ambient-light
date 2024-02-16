c826ceaafe2112c14b2427a8c97e833abe132611
wlad-p <wladpesotskyy@gmail.com>
2024-02-16 14:00:25 +0100

    successfully get image data

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,30 +1,43 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <chrono>
+#include <cstdlib> 
 
 using namespace cv;
 using namespace std;
 
-void initArrays(list<int>& ledsLeft, list<int>& ledsTop, list<int>& ledsRight, list<int>& ledsBottom)
+#define LEDS_LEFT 17
+#define LEDS_TOP 29
+#define LEDS_RIGHT 16
+#define LEDS_BOTTOM 28
+#define LEDS_TOTAL (LEDS_LEFT + LEDS_TOP + LEDS_RIGHT + LEDS_BOTTOM)
+
+void initArrays(list<unsigned int>& ledsLeft, list<unsigned int>& ledsTop, list<unsigned int>& ledsRight, list<unsigned int>& ledsBottom)
 {
-    for(int i=0; i < 17; i++)
-        ledsLeft.push_back(i);
-    for(int i=17; i < 46; i++)
-        ledsTop.push_back(i);
-    for(int i=46; i < 62; i++)
-        ledsRight.push_back(i);
-    for(int i=62; i < 91; i++)
-        ledsBottom.push_back(i);
+    for(int i=0; i < LEDS_LEFT; i++)
+        ledsLeft.push_back(0x00000000);
+    for(int i=0; i < LEDS_TOP; i++)
+        ledsTop.push_back(0x00000000);
+    for(int i=0; i < LEDS_RIGHT; i++)
+        ledsRight.push_back(0x00000000);
+    for(int i=0; i < LEDS_BOTTOM; i++)
+        ledsBottom.push_back(0x00000000);
 }
 
-void showlist(list<int> g)
+void showlist(list<unsigned int> g)
 {
 
-    list<int>::iterator it;
+    list<unsigned int>::iterator it;
     for (it = g.begin(); it != g.end(); ++it)
-        cout << '\t' << *it;
+        cout << '\t' << std::hex << *it;
     cout << '\n';
 }
+
+unsigned int bgrToHex(Vec3b color) {
+    unsigned int hexColor = (color[2] << 16) | (color[1] << 8) | color[0];
+    return 0xFF000000 | hexColor;
+}
+
 
 Vec3b dominantColor(Mat& image)
 {
@@ -34,7 +47,7 @@ Vec3b dominantColor(Mat& image)
     reshapedImage.convertTo(reshapedImage, CV_32FC3);
 
     // Perform k-means clustering to find dominant colors
-    int K = 1; // number of clusters (dominant colors)
+    int K = 4; // number of clusters (dominant colors)
     Mat labels, centers;
     kmeans(reshapedImage, K, labels, TermCriteria(TermCriteria::EPS + TermCriteria::MAX_ITER, 10, 1.0), 1, KMEANS_RANDOM_CENTERS, centers);
 
@@ -46,42 +59,41 @@ Vec3b dominantColor(Mat& image)
     
 }
 
-void leftBorder(Mat& image, list<int>& leds)
+void leftBorder(Mat& image, list<unsigned int>& leds)
 {
 
     int imageWidth = image.size().width;
     int imageHeight = image.size().height;
 
-    int top = 100;
-    int bottom = 500;
+    int top = 20;
+    int bottom = 320;
     int sectionSizeY = (bottom-top) / leds.size();
     int sectionSizeX = imageWidth * 0.25;
 
 
     for( int i=0; i<leds.size(); i++){
         
-        int startRow = top + (i * sectionSizeY);
-        int endRow = top + ((i+1) * sectionSizeY);
+        int startRow = bottom - ((i+1) * sectionSizeY);
+        int endRow = bottom - (i * sectionSizeY);
         int startColumn = 0;
         int endColumn = sectionSizeX;
         Mat cropped_image = image(Range(startRow, endRow), Range(startColumn, endColumn)).clone();
-        Vec3b color = dominantColor(cropped_image);
-
-        cout << "color " << i << " : " << color << endl;
+        Vec3b colorBGR = dominantColor(cropped_image);
         
-        string img_title = to_string(i);
-        //imshow(img_title, cropped_image);       
+        auto it = leds.begin();
+        std::advance(it, i);
+        *it = bgrToHex(colorBGR);    
         
     }
 }
 
-void rightBorder(Mat& image, list<int>& leds)
+void rightBorder(Mat& image, list<unsigned int>& leds)
 {
     int imageWidth = image.size().width;
     int imageHeight = image.size().height;
 
-    int top = 100;
-    int bottom = 500;
+    int top = 20;
+    int bottom = 320;
     int sectionSizeY = (bottom-top) / leds.size();
     int sectionSizeX = imageWidth * 0.25;
 
@@ -93,23 +105,22 @@ void rightBorder(Mat& image, list<int>& leds)
         int startColumn = imageWidth - sectionSizeX;
         int endColumn = imageWidth;
         Mat cropped_image = image(Range(startRow, endRow), Range(startColumn, endColumn)).clone();
-        Vec3b color = dominantColor(cropped_image);
+        Vec3b colorBGR = dominantColor(cropped_image);
 
-        cout << "color " << i << " : " << color << endl;
-        
-        string img_title = to_string(i);
-        //imshow(img_title, cropped_image);       
+        auto it = leds.begin();
+        std::advance(it, i);
+        *it = bgrToHex(colorBGR);  
         
     }
 }
 
-void topBorder (Mat& image, list<int>& leds)
+void topBorder (Mat& image, list<unsigned int>& leds)
 {
     int imageWidth = image.size().width;
     int imageHeight = image.size().height;
 
-    int left = 100;
-    int right = 900;
+    int left = 20;
+    int right = 540;
     int sectionSizeY = imageHeight * 0.25;
     int sectionSizeX = (right-left) / leds.size();
 
@@ -121,22 +132,22 @@ void topBorder (Mat& image, list<int>& leds)
         int startColumn = left + (i * sectionSizeX);
         int endColumn = left + ((i+1) * sectionSizeX);
         Mat cropped_image = image(Range(startRow, endRow), Range(startColumn, endColumn)).clone();
-        Vec3b color = dominantColor(cropped_image);
+        Vec3b colorBGR = dominantColor(cropped_image);
 
-        cout << "color " << i << " : " << color << endl;
-        
-        string img_title = to_string(i);
-        //imshow(img_title, cropped_image);       
+        auto it = leds.begin();
+        std::advance(it, i);
+        *it = bgrToHex(colorBGR);
+          
     }
 }
 
-void bottomBorder (Mat& image, list<int>& leds)
+void bottomBorder (Mat& image, list<unsigned int>& leds)
 {
     int imageWidth = image.size().width;
     int imageHeight = image.size().height;
 
-    int left = 100;
-    int right = 900;
+    int left = 20;
+    int right = 540;
     int sectionSizeY = imageHeight * 0.25;
     int sectionSizeX = (right-left) / leds.size();
 
@@ -148,14 +159,39 @@ void bottomBorder (Mat& image, list<int>& leds)
         int startColumn = left + (i * sectionSizeX);
         int endColumn = left + ((i+1) * sectionSizeX);
         Mat cropped_image = image(Range(startRow, endRow), Range(startColumn, endColumn)).clone();
-        Vec3b color = dominantColor(cropped_image);
+        Vec3b colorBGR = dominantColor(cropped_image);
 
-        cout << "color " << i << " : " << color << endl;
+        //cout << "color " << i << " : " << colorBGR << endl;
         
-        string img_title = to_string(i);
+        auto it = leds.begin();
+        std::advance(it, i);
+        *it = bgrToHex(colorBGR);
+
+        //string img_title = to_string(i);
         //imshow(img_title, cropped_image);       
     }
 }
+
+string buildHexString(const list<unsigned int>& list1,
+                      const list<unsigned int>& list2,
+                      const list<unsigned int>& list3,
+                      const list<unsigned int>& list4) {
+    stringstream ss;
+    for (unsigned int value : list1) {
+        ss << setw(8) << setfill('0') << hex << value << " ";
+    }
+    for (unsigned int value : list2) {
+        ss << setw(8) << setfill('0') << hex << value << " ";
+    }
+    for (unsigned int value : list3) {
+        ss << setw(8) << setfill('0') << hex << value << " ";
+    }
+    for (unsigned int value : list4) {
+        ss << setw(8) << setfill('0') << hex << value << " ";
+    }
+    return ss.str();
+}
+
  
 
 int main(int argc, char** argv )
@@ -176,27 +212,28 @@ int main(int argc, char** argv )
         printf("No image data \n");
         return -1;
     }
-    resize(image, image, Size(), 0.5, 0.5);
+    resize(image, image, Size(580, 340));
 
-    list<int> ledsLeft, ledsTop, ledsRight, ledsBottom;
+    list<unsigned int> ledsLeft, ledsTop, ledsRight, ledsBottom;
     initArrays(ledsLeft, ledsTop, ledsRight, ledsBottom);
-
-
-    cout << "Width : " << image.size().width << endl;
-    cout << "Height: " << image.size().height << endl;
-    cout<<"Channels: :"<< image.channels() << endl;
 
     leftBorder(image, ledsLeft);
     rightBorder(image, ledsRight);
     topBorder(image,ledsTop);
     bottomBorder(image, ledsBottom);
 
+    // Pass to C script
+    string hexString = buildHexString(ledsLeft, ledsTop, ledsRight, ledsBottom);
+    string command = "sudo ./rpi_ws281x/ledtest " + hexString;
+    int result = system(command.c_str());
+
+
     auto current_time = std::chrono::high_resolution_clock::now();
     std::cout << "Program has been running for " << std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count() << "mili seconds" << std::endl;
 
  
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    //namedWindow("Display Image", WINDOW_AUTOSIZE );
+    //imshow("Display Image", image);
     waitKey(0);
     return 0;
 }
